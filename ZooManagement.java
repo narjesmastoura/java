31b98f8d2db01ebe4b07389af5542c90873039e4
narjesmastoura <147508960+narjesmastoura@users.noreply.github.com>
2024-05-09 00:00:47 +0100

    PROSIT1

--- ZooManagement.java
+++ ZooManagement.java
@@ -1,66 +1,28 @@
-package in.esprit.gestionZoo.main;
-import tn.esprit.gestionZoo.entitée.Animal;
-import tn.esprit.gestionZoo.entitée.InvalidAgeException;
-import tn.esprit.gestionZoo.entitée.Zoo;
-import tn.esprit.gestionZoo.entitée.ZooFullException;
-import tn.esprit.gestionZoo.entitée.*;
+import java.util.Scanner;
 
 public class ZooManagement {
-
     public static void main(String[] args) {
-        Animal lion = new Animal("Felidae", "Simba", 5, true);
-        Zoo zoo= new Zoo();
+        // Instruction 1
+        int nbrCages = 20;
+        String zooName = "my zoo";
+        System.out.println(zooName + " comporte " + nbrCages + " cages");
 
-        /*Zoo myZoo = new Zoo("Wildlife Park", "Ariana");
-        Zoo notMyZoo = new Zoo("WaterPark", "Siliana");
-        TerrestrialAnimal terrestrialAnimal = new TerrestrialAnimal();
-        Dolphin dolphin = new Dolphin("Delphinidae", "Flipper", 10, true, "Ocean", 20.5f);
-        Penguin penguin = new Penguin("Spheniscidae", "Tux", 5, true, "Antarctica", 50.0f);
-
-        System.out.println(dolphin);
-
-        System.out.println(penguin);
-
-        System.out.println(terrestrialAnimal);
-
-
-        dolphin.swim();
-        penguin.swim();
-
-        Animal dog = new Animal("Canine", "Snoopy", 2, true);
-
-        System.out.println(myZoo.addAnimal(lion));
-        System.out.println(myZoo.addAnimal(dog));
-
-        myZoo.displayAnimals();
-
-        System.out.println(myZoo.searchAnimal(dog));
-        Animal dog2 = new Animal("Canine", "lll", 2, true);
-        System.out.println(myZoo.searchAnimal(dog2));
-
-        myZoo.displayAnimals();
-
-        System.out.println(myZoo);*/
-        try {
-
-        Zoo.addAnimal(new Animal("lion","loulou",-5));
-        Zoo.addAnimal(new Animal("dog","doudou",5);
-        Zoo.addAnimal(new Animal("dog2","joujou",2);
-
-        /*myZoo.displayAnimals();
-        System.out.println("a" + myZoo.removeAnimal(lion));
-        myZoo.displayAnimals();
-        System.out.println("a" + myZoo.removeAnimal(dog2));
-        myZoo.displayAnimals();
-        System.out.println("a" + myZoo.removeAnimal(dog));
-        myZoo.displayAnimals();*/
-
-//        System.out.println(tn.esprit.gestionzoo.entitees.Zoo.comparerZoo(myZoo, notMyZoo));
-//        System.out.println(myZoo.isZooFull());
-
-    } catch (ZooFullException | InvalidAgeException ex)
-        {
-            System.out.println("Erreur:"+ ex.getMessage());
+        // Instruction 2
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entrez le nombre de cages :");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Veuillez entrer un nombre valide :");
+            scanner.next();
         }
+        nbrCages = scanner.nextInt();
+
+        System.out.println("Entrez le nom du zoo :");
+        zooName = scanner.next();
+
+        System.out.println("Nombre de cages : " + nbrCages);
+        System.out.println("Nom du zoo : " + zooName);
+
+        // Instruction 3
+        System.out.println("Changements effectués : " + zooName + " comporte " + nbrCages + " cages");
     }
 }
