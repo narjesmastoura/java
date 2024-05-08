836c2293fca2ae7f2a38f521b37ff15dcfbe56f8
narjesmastoura <147508960+narjesmastoura@users.noreply.github.com>
2024-05-08 23:34:51 +0100

    Prosit9

---
+++ IGestion.java
@@ -0,0 +1,7 @@
+public interface IGestion<T> {
+    void ajouterEmploye(T t);
+    boolean rechercherEmploye(String nom);
+    boolean rechercherEmploye(T t);
+    void supprimerEmploye(T t);
+    void displayEmploye();
+}
